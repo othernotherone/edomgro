d98276fae06740191387abe9f618890b5e1ada85
Jake Levine <80796696+othernotherone@users.noreply.github.com>
2024-10-30 22:39:35 -0400

    Add basic main rust file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,22 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use tauri::Manager;
+
+// This is where we'll add our org-mode parser later
+mod org_parser;
+
+// Basic health check command
+#[tauri::command]
+fn ping() -> String {
+    "pong".into()
+}
 
 fn main() {
-    edomgro_lib::run()
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![ping])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
